0890a56d86d8c44a6811c286fd42798ba2ccb13f
Alex Wang <wxb_nudt@163.com>
2018-11-09 19:54:49 +0800

    commit authorization_withdb module.

---
+++ authorization_withdb/src/main/java/com/apkkids/AuthorizationWithdbApplication.java
@@ -0,0 +1,12 @@
+package com.apkkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationWithdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationWithdbApplication.class, args);
+	}
+}
